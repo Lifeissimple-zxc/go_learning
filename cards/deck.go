618ebaae994f8ce1f73f59fa38b1932f42bba034
Lifeissimple-zxc <mararkarp@gmail.com>
2023-05-24 00:29:19 +0200

    Continued working on cards project

--- cards/deck.go
+++ cards/deck.go
@@ -1,12 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Creating a new custom deck type (it is a slice of strings)
 type deck []string // It kinda extends the behavior of string Slice
+
+func newDeck() deck {
+	cards := deck{}
+
+	// Inputs for our combinations
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// Iterative generation of cards
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+
+		}
+	}
+
+	return cards
+}
 
 func (d deck) print() { // (d deck) means receiver of type deck
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// Return multiple values from the function
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	// Type conversion & Join string slice to a string
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(
+		filename,
+		[]byte(d.toString()),
+		0666,
+	)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,22 @@
 // This means it is executable, not reausable
 package main
 
+import "fmt"
+
+const CARDS_FILE_NAME string = "cards_deck.txt"
+
 // Needed for main package for code to be executed
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	// Iterate over cards
-	cards.print() // Looks like OOP?
-}
+	hand, remainingDeck := deal(cards, 5)
 
-func newCard() string {
-	return "Five of Diamonds"
+	fmt.Println("Hand is")
+	hand.print()
+
+	fmt.Println("Remaining is")
+	remainingDeck.print()
+
+	fmt.Println("Saving cards to file")
+	cards.saveToFile(CARDS_FILE_NAME)
 }
