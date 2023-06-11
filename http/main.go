f2b055f5f9fe7e2929f8030ef92694755fe25a81
Lifeissimple-zxc <mararkarp@gmail.com>
2023-06-11 17:34:36 +0200

    Parsed http response body to a string using a Reader interface

--- http/main.go
+++ http/main.go
@@ -16,5 +16,13 @@ func main() {
 	}
 
 	// Naive way of printing our response from the server
-	fmt.Println(resp)
+	// fmt.Println(resp)
+
+	// Proper logging of the response body to the command line
+	// We set a size of our slice because Read does not resize our slice
+	bs := make([]byte, 99999) // give us an empty byte slice with 99999 elements
+
+	resp.Body.Read(bs) // This parses our body
+	// Log our HTML response
+	fmt.Println(string(bs))
 }
