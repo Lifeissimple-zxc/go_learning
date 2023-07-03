d48e1cf38277258dc3f60e7972e5c77efc745c60
Lifeissimple-zxc <mararkarp@gmail.com>
2023-07-03 23:33:47 +0200

    Updated server1 program to test it on GCP machine

--- gopl_book/ch1/server1/main.go
+++ gopl_book/ch1/server1/main.go
@@ -5,11 +5,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
+const PORT = "8080"
+
 func main() {
+
+	if len(os.Args) == 1 {
+		fmt.Fprintf(os.Stderr, "Usage: programm.go r | l\n")
+		os.Exit(1)
+	}
+
+	env := strings.ToLower(os.Args[1])
+
+	if !(env == "r" || env == "l") {
+		fmt.Fprintf(os.Stderr, "Unexpected env: %s\n", env)
+		os.Exit(1)
+	}
+
+	var servers = map[string]string{
+		"r": "",
+		"l": "localhost",
+	}
+
+	server := servers[env] + ":" + PORT
+
 	http.HandleFunc("/", handler) // each request calls our handler func defined below
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe(server, nil))
 }
 
 // Handler echoes the path component ofm the request URL
