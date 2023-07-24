6344c9473d2dc305fea1af9c6c42eda978712ddd
Lifeissimple-zxc <mararkarp@gmail.com>
2023-07-24 23:56:39 +0200

    Implemented a url shortener following the gophercises course

--- gophercises/url_shortener/handler.go
+++ gophercises/url_shortener/handler.go
@@ -1,7 +1,10 @@
 package urlshort
 
 import (
+	"fmt"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,12 +14,20 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	// Idea
-	// for path, url := range
-	// if url == "": use fallback handler
-	// else create a custom handler with url
-	return nil
+	fmt.Println("pathsToUrls:", pathsToUrls)
+	// Func that matches http.HandlerFunc signature: https://pkg.go.dev/net/http#HandleFunc
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Logic: if we match a path --> redirect,
+		// else --> fallback
+		path := r.URL.Path
+		// Oneliner for variable init + control flow
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			// Exit the func at this point
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -36,6 +47,26 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	// Parse the yaml
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yml, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	// Convert yaml array to a map
+	// Create a map-container of our data
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		// Write data from our array of structs to the map
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	// return a map hander using the map
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+// Custom type for parsing yaml, uses tags
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
