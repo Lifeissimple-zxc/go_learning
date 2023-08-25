c51699306b7ca98dad900fedc989825b6ac16fd0
Lifeissimple-zxc <mararkarp@gmail.com>
2023-08-26 00:09:21 +0200

    uncommited changes?

--- gophercises/cyoa/this_renders_css.go
+++ gophercises/cyoa/this_renders_css.go
@@ -6,13 +6,34 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
+var tmpl *template.Template
+
 func main() {
 
-	// open file
+	mux := http.NewServeMux()
+	tmpl = template.Must(template.ParseFiles("templates/arc.html"))
+
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+	mux.HandleFunc("/intro", showStory)
+
+	log.Fatal(http.ListenAndServe(":9091", mux))
+
+}
+
+func showStory(w http.ResponseWriter, r *http.Request) {
+	// Generate HTML
+	htmlBytes, err := readFile("templates/arc.html")
+	if err != nil {
+		fmt.Printf("Err opening html file. Details: %v\n", err)
+		os.Exit(1)
+	}
+
 	file, err := readFile(story.STORY_PATH)
 	if err != nil {
 		fmt.Printf("Err opening file %s. Details: %v\n", story.STORY_PATH, err)
@@ -27,25 +48,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Generate HTML
-	htmlBytes, err := readFile("templates/arc.html")
-	if err != nil {
-		fmt.Printf("Err opening html file. Details: %v\n", err)
-		os.Exit(1)
-	}
-	// parse to Story struct
 	var st story.Story
 	st.Init(storyData, htmlBytes)
 
-	mux := http.NewServeMux()
-	tmpl := template.Must(template.ParseFiles("templates/arc.html"))
-	fs := http.FileServer(http.Dir("./static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fs))
-	mux.HandleFunc("/todo", todo)
-
+	// Execute template
+	tmpl.Execute(w, st.Arcs["intro"])
 }
-
-func showStory(name string, st *story.Story, tmpl *template.Template)
 
 // readFile reads a file to a bytes slice
 func readFile(path string) ([]byte, error) {
